428859d0c41b11b4004d811a23d408e4781ca770
hantze <hantze.sudarma@payfazz.com>
2018-10-18 12:40:10 +0700

    change pusher sh

--- internal/pusher/sh-tmpl.go
+++ internal/pusher/sh-tmpl.go
@@ -2,14 +2,26 @@ package pusher
 
 var template = `#!/bin/sh
 
-set -eu
+set -u
 
-docker tag $1 ${deployer}${server}/$1
+panic() {
+  panic_code=$1
+  shift
+  echo "$@" >&2
+  exit $panic_code
+}
+
+docker tag $1 ${deployer}${server}/$1 || panic $? "cannot tag"
 
 ssh -NT -L 5000:${server} ${ssh} &
 ssh_pid=$!
 
-docker push ${deployer}${server}/$1
+sleep 1 
+
+exit_code=0
+docker push ${deployer}${server}/$1 || exit_code=$?
 
 kill $ssh_pid
+
+exit $exit_code
 `
