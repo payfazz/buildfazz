92a8d137a8441278f23519561f49ee0ad0f1142e
Yahya <lvnp@users.noreply.github.com>
2018-12-17 16:48:04 +0700

    Reimplement pushergenerator

--- internal/pusher/pushergenerator.go
+++ internal/pusher/pushergenerator.go
@@ -1,14 +1,13 @@
 package pusher
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/payfazz/buildfazz/internal/builder"
-	"io"
 	"log"
+	"net"
 	"os"
 	"os/exec"
-	"strings"
+	"time"
 )
 
 // Generator ...
@@ -21,59 +20,102 @@ type Generator struct {
 	ssh         string
 }
 
-func (g *Generator) generateSh() {
-	var replacer = strings.NewReplacer("${deployer}", g.deployer, "${server}", g.server, "${ssh}", g.ssh)
-	g.shPath = "pusher.sh"
-	if _, err := os.Stat(g.shPath); !os.IsNotExist(err) {
-		os.Remove(g.shPath)
+// Create new docker tag
+func (g *Generator) createTag() string {
+	oldtag := fmt.Sprintf("%s:%s", g.projectName, g.projectTag)
+	newtag := fmt.Sprintf("%s%s/%s", g.deployer, g.server, oldtag)
+
+	cmd := exec.Command("docker", "tag", oldtag, newtag)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("failed to create docker tag: %s\n", err)
 	}
-	fo, _ := os.Create(g.shPath)
-	defer func() {
-		if err := fo.Close(); err != nil {
-			log.Fatalf("can't create file %s, err : %s", g.shPath, err)
-		}
-	}()
-	builderScript := replacer.Replace(template)
-	if _, err := fo.Write([]byte(builderScript)); err != nil {
-		log.Fatalf("error while writting file %s, err : %s", g.shPath, err)
-	}
-	os.Chmod(g.shPath, 0755)
+
+	log.Printf("created docker tag %s\n", newtag)
+	return newtag
 }
 
-func (g *Generator) clearFiles() {
-	os.Remove(g.shPath)
+// Mash port until it's open, otherwise return error
+func waitPort(proto, address string, duration time.Duration) error {
+	var (
+		err  error
+		conn net.Conn
+	)
+	timeLimit := time.Now().Add(duration)
+	for {
+		conn, err = net.Dial(proto, address)
+		if err == nil || time.Now().After(timeLimit) {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	if conn != nil {
+		return nil
+	} else {
+		return err
+	}
 }
 
-func (g *Generator) execSh() {
-	var stderrBuf bytes.Buffer
-	proj := fmt.Sprintf("%s:%s", g.projectName, g.projectTag)
-	cmd := exec.Command("/bin/sh", g.shPath, proj)
-	stderrIn, err := cmd.StderrPipe()
-	stderr := io.MultiWriter(os.Stderr, &stderrBuf)
-	err = cmd.Start()
-	if err != nil {
-		log.Fatalf("cmd.Start() failed with '%s'\n", err)
+// Starts local SSH tunnel in port 5000 to the registry
+func (g *Generator) startTunnel() *exec.Cmd {
+	tun := fmt.Sprintf("5000:%s", g.server)
+	cmd := exec.Command("ssh", "-NTL", tun, g.ssh)
+
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("failed to create tunnel: %s\n", err)
+	} else {
+		log.Printf("starting tunnel to %s\n", g.ssh)
 	}
-	go func() {
-		io.Copy(stderr, stderrIn)
-	}()
-	err = cmd.Wait()
+
+	timeout := 10 * time.Second
+	err := waitPort("tcp", "localhost:5000", timeout)
 	if err != nil {
-		if err, ok := err.(*exec.ExitError); ok {
-			log.Fatalf("%#v\n", string(err.Error()))
-		}
-		log.Fatalf("cmd.Run() failed with %s\n", err)
+		log.Fatalf("creating tunnel timed out after %v\n", timeout)
 	}
+
+	log.Println("started tunnel")
+
+	return cmd
+}
+
+// Stops SSH tunnel
+func (g *Generator) stopTunnel(cmd *exec.Cmd) {
+	if err := cmd.Process.Kill(); err != nil {
+		log.Fatalf("failed to stop SSH tunnel: %s\n", err)
+	}
+
+	log.Printf("stopped tunnel to %s\n", g.ssh)
+}
+
+// Push tag to registry
+func (g *Generator) pushTag(tag string) {
+	cmd := exec.Command("docker", "push", tag)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Printf("failed to push tag %s: %s", tag, err)
+	} else {
+		log.Printf("pushed tag %s", tag)
+	}
+}
+
+func (g *Generator) execCommands() {
+	tag := g.createTag()
+	tun := g.startTunnel()
+	g.pushTag(tag)
+	defer g.stopTunnel(tun)
+
 }
 
 // Start ...
 func (g *Generator) Start() {
-	g.generateSh()
 	fmt.Printf("\n\nWARNING, DO NOT CLOSE YOUR APPLICATION!\nYOUR APPS WILL STUCK IF YOU DO THAT!\nDOCKER PUSH ON PROGRESS\n\n")
-	g.execSh()
+	g.execCommands()
 
 	defer func() {
-		g.clearFiles()
 		fmt.Println("PUSH SUCCESS\nImages ", g.projectName, ":", g.projectTag, " pushed to : ", g.ssh)
 		os.Exit(0)
 	}()
