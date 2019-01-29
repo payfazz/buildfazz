434be4372163c72af1cedb2c5c7c27f14ba63a72
lvnp <anakaiti@users.noreply.github.com>
2019-01-29 14:17:18 +0700

    build: fix help message indentation

--- internal/help/build.go
+++ internal/help/build.go
@@ -9,10 +9,10 @@ func (b *Build) GenerateHelp() string {
 Usage: buildfazz build [OPTIONS] {docker-name}:[docker-tag]
 
 Options:	
-	-p			Set buildfazz working directory, default: current directory
-	-os			Set buildfazz default OS (options: debian/ubuntu/scratch/etc...), default: debian
-	-n		  	Do not add git ref suffix to tag
-	--generate	Generate Dockerfile only, don't build.
+	-p          Set buildfazz working directory, default: current directory
+	-os         Set buildfazz default OS (options: debian/ubuntu/scratch/etc...), default: debian
+	-n          Do not add git ref suffix to tag
+	--generate  Generate Dockerfile only, don't build.
 `
 }
 
